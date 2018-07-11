402850f683babb5551230c85519ddd4902255c2c
Somtochukwu Nweke <somtonweke1@fb.com>
2018-07-11 12:29:31 -0700

    Initial Commit

---
+++ Instagram/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Instagram
+//
+//  Created by Somtochukwu Nweke on 7/11/18.
+//  Copyright © 2018 Somtochukwu Nweke. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Instagram/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Instagram
+//
+//  Created by Somtochukwu Nweke on 7/11/18.
+//  Copyright © 2018 Somtochukwu Nweke. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
